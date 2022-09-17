f9acefdde0fdb2cca3ad44cf7dd29b6e21e96d55
BigElmo <bigelmogm@gmail.com>
2022-09-17 19:07:34 +1000

    task1 done (Builder pattern for Person class)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person ivan = new Person.Builder("Ivan", "Petrov", "Male")
+                .withMiddleName("Olegovich")
+                .withAge(22)
+                .build();
+
+        Person alex = new Person.Builder("Alex", "Sultanov", "Male")
+                .build();
+
+        System.out.println(ivan);
+        System.out.println(alex);
     }
 }
